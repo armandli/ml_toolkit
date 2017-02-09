becc487c58233ed4daba3557d43010a65c5f918c
armandli <armand.li@hotmail.com>
2017-02-09 00:29:51 -0500

    fully clean up the ml_util header

--- eigen/ml_util.h
+++ eigen/ml_util.h
@@ -1,26 +1,4 @@
 #ifndef ML_UTIL
 #define ML_UTIL
 
-#include <cmath>
-#include <ctime>
-#include <random>
-#include <Eigen/Dense>
-
-using namespace std;
-using Eigen::MatrixXd;
-
-enum MatrixDim {
-  RowDim,
-  ColDim,
-};
-
-MatrixXd random_matrix(int rows, int cols){
-  normal_distribution<double> dist(0.0, 0.5);
-  default_random_engine eng(time(0));
-  MatrixXd ret(rows, cols);
-
-  ret = ret.unaryExpr([&dist, &eng](double){ return dist(eng); });
-  return ret;
-}
-
 #endif
